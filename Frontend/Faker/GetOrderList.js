15ee2f34357f85be239637d6e5cea018f6931cf2
KadamQ <kovadam95@gmail.com>
2020-03-10 23:05:42 +0100

    Reconsider my code, then correct my mistakes and misunderstandings based on the REST API documentation

--- Frontend/Faker/GetOrderList.js
+++ Frontend/Faker/GetOrderList.js
@@ -1,29 +1,36 @@
 const faker = require('faker');
 const fs = require('fs');
 
-function generatePizzas() {
-    return {
-        sequentialNumber : faker.random.number(200),
-        pizzaName : faker.lorem.word(),
-        isDone : faker.random.boolean()
+function generatePizza() {
+    let pizzas = [];
+    const PIZZAS_COUNT = Math.floor(Math.random() * 5) + 1; //1-5 között ad vissza egy számot
+    for (let i = 0; i < PIZZAS_COUNT; i++) {
+        let pizza = {
+            pizzaName : faker.lorem.word(),
+            isDone : faker.random.boolean()
+        }
+        pizzas[i] = pizza;
+        pizzas[i]['sequentialNumber'] = i + 1;
     }
-}
-let pizzas = [];
-const PIZZA_COUNT = 100;
-for (let i = 0; i < PIZZA_COUNT; i++) {
-    pizzas[i] = generatePizzas();
-    pizzas[i]['id'] = i;
+    return pizzas;
 }
 
-function GetOrderList() {
-    let Pizza = {
+function generateOrder(){
+    let order = {
         listId : faker.random.uuid(),
+        pizza : generatePizza()
     }
-    return Pizza;
+    return order;
 }
 
-let db = (
-    pizzas = pizzas
-)
-console.log(GerOrderList());
-fs.writeFileSync('D:/Faker/getOrderList_database.json', JSON.stringify((db)));
+function generateOrderList() {
+    let orders= [];
+    const ORDERS_COUNT = 100;
+    for (let i = 0; i < ORDERS_COUNT; i++) {
+        orders[i] = generateOrder()
+    }
+    return orders;
+}
+
+console.log(generateOrderList());
+fs.writeFileSync('D:/Faker/GetOrderList_database.json', JSON.stringify((generateOrderList())));
